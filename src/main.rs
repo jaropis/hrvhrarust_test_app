07db7d20ca151ace8b58e16b6145c75b37c33862
Jaroslaw Piskorski <jaropis@zg.home.pl>
2025-07-13 21:51:27 +0200

    a new version with runs variances

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use hrvhra_rust::{data_reader::RRSeries, runs::RRRuns};
 
 fn main() {
     // Method 1: Direct vector input
-    let rr_data = vec![1.0, 2.0, 3.0, 2.0, 1.0];
-    let annot_data = vec![0, 0, 0, 0, 0];
+    let rr_data = vec![1.0, 2.0, 3.0, 2.0, 1.0, 2.0, 3.0, 4.0, 1.0];
+    let annot_data = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
     let mut rr = RRRuns::new(rr_data, annot_data, true);
     rr.get_full_runs();
     let summary = rr.get_runs_summary();
     println!("Analysis results: {:?}", summary);
+    rr.print_runs_addresses();
+    rr.calculate_runs_variances();
+    rr.print_runs_variances();
 
     // Method 2: Reading from file
     match RRSeries::read_rr("test_longer.csv") {
@@ -18,8 +21,11 @@ fn main() {
             let summary = rr.get_runs_summary();
             println!("File analysis results: {:?}", summary);
             let signal = rr_series.rr.clone();
-            let sampen = samp_en::calc_samp_en(&signal, 0.15);
+            let sampen = samp_en::calc_samp_en(&signal, 2, 0.15);
             println!("SampEn: {}", sampen);
+            // rr.print_runs_addresses();
+            rr.calculate_runs_variances();
+            rr.print_runs_variances();
         }
         Err(e) => println!("Error reading file: {}", e),
     }
